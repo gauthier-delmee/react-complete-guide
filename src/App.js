270d8e4c5ebce63e5e55ed72035718ceec0c8ede
Gauthier Delmee <gauthier.delmee@cyara.com>
2018-11-20 09:46:28 +1100

    Refactored toggle names code.

--- src/App.js
+++ src/App.js
@@ -46,6 +46,27 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer',
     }
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Elle!')}
+            changed={this.nameChangedHandler}>My Hobbies: racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
@@ -53,21 +74,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Names</button>
-        { this.state.showPersons ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Elle!')}
-              changed={this.nameChangedHandler}>My Hobbies: racing</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div> : null
-        }
+        {persons}
       </div>
     );
   }
