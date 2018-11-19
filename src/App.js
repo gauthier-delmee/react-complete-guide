a358afac511a19e0ec43b6266357656a0e31fdd5
Gauthier Delmee <gauthier.delmee@cyara.com>
2018-11-20 10:00:19 +1100

    Changed handler on Persons to delete the person if it is clicked.

--- src/App.js
+++ src/App.js
@@ -12,15 +12,10 @@ class App extends Component {
     showPersons: false,
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Gauthier', age: 42 },
-        { name: 'Stephanie', age: 31 },
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   nameChangedHandler = (event) => {
@@ -52,8 +47,9 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person
+              click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age} />
           })}
