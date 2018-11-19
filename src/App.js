c4880df8f7ec92f0e95802c1410a968ae18539b5
Gauthier Delmee <gauthier.delmee@cyara.com>
2018-11-19 16:34:17 +1100

    added the Person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <h1>Hi I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="32">My hobbies: Racing</Person>
       </div>
     );
   }
