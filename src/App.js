0bbe0bdaa865bf5fb3edf6104b7dbede4bf677ae
Gauthier Delmee <gauthier.delmee@cyara.com>
2018-11-19 20:44:26 +1100

    Added state and switchNameHandler.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Gauthier', age: 42 },
+      { name: 'Stephanie', age: 31 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: 'Eleonora', age: 28 },
+        { name: 'Gauthier', age: 42 },
+        { name: 'Stephanie', age: 31 },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="32">My hobbies: Racing</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
