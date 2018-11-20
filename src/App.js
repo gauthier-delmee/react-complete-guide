f603b4e614ca06672a5873d051893cd86f38b775
Gauthier Delmee <gauthier.delmee@cyara.com>
2018-11-20 18:15:41 +1100

    Added the Radium package and implemented pseudo selectors to change the hover state of the toggle button.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
+
 import Person from './Person/Person';
 
 class App extends Component {
@@ -49,7 +51,11 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-    }
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    };
 
     let persons = null;
 
@@ -68,6 +74,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -91,4 +101,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
