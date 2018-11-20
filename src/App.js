ac361f3c482dd9c0f8da71c502542b2bd0a9e62e
Gauthier Delmee <gauthier.delmee@cyara.com>
2018-11-20 21:45:40 +1100

    Reverted the Radium implementation.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 
 import Person from './Person/Person';
 
@@ -51,10 +50,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
@@ -74,10 +69,6 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     const classes = [];
@@ -89,18 +80,16 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Names</button>
-          {persons}
-        </div>
-    </StyleRoot>
+      <div className="App">
+        <h1>Hi I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Names</button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
