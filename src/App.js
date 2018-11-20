3431ca9d82e78d78e64be5e9375e2222ac9d1c7b
Gauthier Delmee <gauthier.delmee@cyara.com>
2018-11-20 17:37:15 +1100

    Refactored the onChange method of the text input in the Person list elements.

--- src/App.js
+++ src/App.js
@@ -19,15 +19,22 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  // nameChangedHandler = (event) => {
-  //   this.setState({
-  //     persons: [
-  //       { name: 'Max', age: 28 },
-  //       { name: event.target.value, age: 42 },
-  //       { name: 'Stephanie', age: 31 },
-  //     ]
-  //   })
-  // }
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.person.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} )
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -53,7 +60,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
